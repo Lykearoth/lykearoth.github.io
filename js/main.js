ccc00d8905e3fe13f3fd2f9cecba8de68eef22e7
Lykearoth <120321807+Lykearoth@users.noreply.github.com>
2024-08-31 16:25:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Kea Rorh.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["KeaRoth Ly.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
       typeSpeed: 100,
       loop: true,
     });
